40d846fa7250dba753d5f3c786232f68dc17c1be
fudo <saxsax199502a@gmail.com>
2023-04-05 09:15:45 +0700

    update

--- auth_custom/commands/createsuperuser.py
+++ auth_custom/commands/createsuperuser.py
@@ -3,4 +3,7 @@ from django.contrib.auth.management.commands.createsuperuser import Command as S
 
 class Command(SuperUserCommand):
     def handle(self, *args, **options):
+        # i'm trying to override "python3 manage.py createsuperuser" command
+        # the purpose is whenever super user created by that command,
+        # i'll create record in table quiz_administrator as well.
         a = 1
